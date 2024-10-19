39d54502bcd73aaf0794b96f3278eac607fef4fa
OscarSalasF19 <oscar19082005@gmail.com>
2024-10-19 15:20:45 -0600

    Creation of Class Button, and display palette
    Creating a dedicated class for buttons, and ajusting the code to display the color palette

--- Menu.cpp
+++ Menu.cpp
@@ -2,16 +2,13 @@
 
 
 Menu::Menu() {
-	isTheAddActive = false;
-	isTheDeleteActive = false;
 	initializeWindow();
 	intializeMap();
 	intializeAddButton();
 	intializeDeleteButton();
 	intializeLoadButton();
+	initializeColorPalette();
 	displayWindow();
-	displayColorPalette();
-
 }
 
 Menu::~Menu() {
@@ -21,93 +18,57 @@ Menu::~Menu() {
 void Menu::displayWindow() {
 	while (window->isOpen()) {
 		while (window->pollEvent(evnt)) {
-			EventConditions();
+			eventScenarios();
 		}
-		displayMenu();
-
+		window->clear(sf::Color::White);
+		window->draw(map);
+		window->draw(AddPoints->displayButton());
+		window->draw(deletePoints->displayButton());
+		window->draw(loadPoints->displayButton());
+		if (AddPoints->getStatus() == true) {
+			displayColorPalette();
+		}
+		window->display();
 	}
 }
 
 void Menu::loadTexture() {
 	mapTexture.loadFromFile("Map_Of_Proy.jpeg");
 	map.setTexture(&mapTexture);
-	map.setScale(1.0f, 0.79f);
+	map.setScale(1.f, 0.79f);
 }
 
 void Menu::initializeWindow() {
-	window = new sf::RenderWindow(sf::VideoMode(1200, 1000), "Route designer", sf::Style::Titlebar | sf::Style::Close);
+	window = new sf::RenderWindow(sf::VideoMode(1200, 1000), "Route Desinger", sf::Style::Titlebar | sf::Style::Close);
 }
 
 void Menu::intializeMap() {
-	map = sf::RectangleShape(sf::Vector2f(800.0f, 1280.0f));
+	map = sf::RectangleShape(sf::Vector2f(800.f, 1280.f));
 	loadTexture();
 }
-
 void Menu::intializeAddButton() {
-	addPointButton = sf::RectangleShape(sf::Vector2f(300, 100));
-	addPointButton.setPosition(sf::Vector2f(850, 100));
-	addingPointsTexture.loadFromFile("add_points.png");
-	addPointButton.setTexture(&addingPointsTexture);
+	AddPoints = new Button("add_points.png", sf::Vector2f(300, 100), sf::Vector2f(850, 100));
 }
 
 void Menu::intializeDeleteButton() {
-	deletePointButton = sf::RectangleShape(sf::Vector2f(300, 100));
-	deletePointButton.setPosition(sf::Vector2f(850, 300));
-	deletingPointsTexture.loadFromFile("Delete_Button.png");
-	deletePointButton.setTexture(&deletingPointsTexture);
-}
+	deletePoints = new Button("Delete_Button.png", sf::Vector2f(300, 100), sf::Vector2f(850, 300));
 
+}
 void Menu::intializeLoadButton() {
-	loadRouteButton = sf::RectangleShape(sf::Vector2f(300, 100));
-	loadRouteButton.setPosition(sf::Vector2f(850, 500));
-	loadingPointsTexture.loadFromFile("Load_button.png");
-	loadRouteButton.setTexture(&loadingPointsTexture);
+	loadPoints = new Button("Load_Button.png", sf::Vector2f(300, 100), sf::Vector2f(850, 500));
 
 }
 
 void Menu::initializeColorPalette() {
-	palette = new sf::RectangleShape(sf::Vector2f(300, 300));
-	sf::Texture* paletteTexture = new sf::Texture;
-	paletteTexture->loadFromFile("color_palette.png");
-	palette->setTexture(paletteTexture);
-	palette->setPosition(sf::Vector2f(830.f, 668.f));
+	Palette = new Button("color_palette.png", sf::Vector2f(300.f, 300.f), sf::Vector2f(830.f, 668.f));
 }
 
 void Menu::displayColorPalette() {
-	while (isTheAddActive && !isTheDeleteActive) {
-		window->clear(sf::Color::White);
-		window->draw(map);
-		window->draw(addPointButton);
-		window->draw(deletePointButton);
-		window->draw(loadRouteButton);
-		window->draw(*palette);
-		window->display();
-	}
+	window->draw(Palette->displayButton());
+	window->display();
 }
 
-void Menu::pressAddButton(sf::Vector2i mousePosition) {
-	if (((mousePosition.x >= 860) && (mousePosition.x <= 1145)) && ((mousePosition.y >= 100) && (mousePosition.y <= 195))) {
-		isTheAddActive = true;
-		isTheDeleteActive = false;
-		std::cout << "adding button\n";
-	}
-}
-
-void Menu::pressDeleteButton(sf::Vector2i mousePosition) {
-	if (((mousePosition.x >= 850) && (mousePosition.x <= 1145)) && ((mousePosition.y >= 300) && (mousePosition.y <= 400))) {
-		isTheAddActive = false;
-		isTheDeleteActive = true;
-		std::cout << "deleting button\n";
-	}
-}
-
-void Menu::pressLoadButton(sf::Vector2i mousePosition) {
-	if (((mousePosition.x >= 850) && (mousePosition.x <= 1145)) && ((mousePosition.y >= 500) && (mousePosition.y <= 600))) {
-		std::cout << "loading Route\n";
-	}
-}
-
-void Menu::EventConditions() {
+void Menu::eventScenarios() {
 	switch (evnt.type) {
 	case sf::Event::Closed:
 		window->close();
@@ -118,19 +79,29 @@ void Menu::EventConditions() {
 			pressAddButton(mousePosition);
 			pressDeleteButton(mousePosition);
 			pressLoadButton(mousePosition);
-
-			std::cout << "( " << mousePosition.x << ", " << mousePosition.y << ")\n";
 		}
 	}
 }
 
-void Menu::displayMenu() {
-	if (!isTheAddActive && !isTheDeleteActive) {
-		window->clear(sf::Color::White);
-		window->draw(map);
-		window->draw(addPointButton);
-		window->draw(deletePointButton);
-		window->draw(loadRouteButton);
-		window->display();
+void Menu::pressAddButton(sf::Vector2i mousePosition) {
+	if (((mousePosition.x >= 860) && (mousePosition.x <= 1145)) && ((mousePosition.y >= 100) && (mousePosition.y <= 195))) {
+		AddPoints->setStatus(true);
+		deletePoints->setStatus(false);
+		std::cout << "add Button" << std::endl;
+	}
+	std::cout << "( " << mousePosition.x << ", " << mousePosition.y << ")\n";
+}
+
+void Menu::pressDeleteButton(sf::Vector2i mousePosition) {
+	if (((mousePosition.x >= 850) && (mousePosition.x <= 1145)) && ((mousePosition.y >= 300) && (mousePosition.y <= 400))) {
+		AddPoints->setStatus(false);
+		deletePoints->setStatus(true);
+		std::cout << "deleting button\n";
+	}
+}
+
+void Menu::pressLoadButton(sf::Vector2i mousePosition) {
+	if (((mousePosition.x >= 850) && (mousePosition.x <= 1145)) && ((mousePosition.y >= 500) && (mousePosition.y <= 600))) {
+		std::cout << "loading Route\n";
 	}
 }
