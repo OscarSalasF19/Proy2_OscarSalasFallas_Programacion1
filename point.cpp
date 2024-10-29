615501d5fbc6ce5c4f4d48ebca34fccb475f7076
OscarSalasF19 <oscar19082005@gmail.com>
2024-10-28 22:56:03 -0600

    restructuring point class and creating class route
    the class point was restructured to work as a node, and the class route was structured as a list of points, and as a node for it's own list

--- point.cpp
+++ point.cpp
@@ -1,26 +1,53 @@
-#include "point.h"
+#include "Point.h"
+Point::Point(sf::Vector2f pointPosition, int pointColor[]) {
+	point = new sf::CircleShape(10.f);
+	next = nullptr;
+	prev = nullptr;
+	for (int i = 0; i < 3; i++) {
+		this->pointColor[i] = pointColor[i];
+	}
+	this->pointPosition = pointPosition;
 
+	point->setPosition(this->pointPosition);
+	point->setOrigin(point->getRadius(), point->getRadius());
+	point->setFillColor(sf::Color(this->pointColor[0], this->pointColor[1], this->pointColor[2]));
+}
 
-Point::Point(sf::Vector2f newPosition, sf::Color newColor) {
-	pointPosition = newPosition;
-	pointColor = newColor;
-	initializePoint();
+Point::Point() {
+	point = nullptr;
+	next = nullptr;
+	prev = nullptr;
+	for (int i = 0; i < 3; i++) {
+		pointColor[i] = 0;
+	}
+	pointPosition.x = 0;
+	pointPosition.y = 0;
+
 }
 
 Point::~Point() {
-
 }
 
-void Point::initializePoint() {
-	point = sf::CircleShape(10.f);
-	point.setFillColor(pointColor);
-	point.setPosition(pointPosition);
+void Point::setNext(Point* newPoint) {
+	next = newPoint;
 }
 
-void Point::SetColor(sf::Color newColor) {
-	pointColor = newColor;
+void Point::setPrev(Point* newPoint) {
+	prev = newPoint;
 }
 
-sf::CircleShape Point:: getPoint() {
-	return point;
+Point* Point::getNext() {
+	return next;
+}
+
+Point* Point::getPrev() {
+	return prev;
+}
+
+sf::Vector2f Point::getPosition() {
+	return point->getPosition();
+}
+
+void Point::drawPoint(sf::RenderWindow* window) {
+	window->draw(*point);
 }
